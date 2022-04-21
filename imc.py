8ee5da3c582b8a6587357bd6afc3e6b15369d538
CleilsonAndrade <cleilsonjose@hotmail.com>
2022-04-21 14:23:49 -0300

    added new exercises

---
+++ imc.py
@@ -0,0 +1,6 @@
+peso = float(input("Digite o peso: "))
+altura = float(input("Digite a altura: "))
+
+imc = peso / (altura**2)
+
+print(f"IMC: {imc:.2f}")
---
+++ ler_2_nums_relizar_operacoes.py
@@ -0,0 +1,10 @@
+n1 = int(input("Digite um n1: "))
+n2 = int(input("Digite um n2: "))
+
+print("Soma:",(n1+n2))
+
+print("Subtração:",(n1-n2))
+
+print("Multiplicação:",(n1*n2))
+
+print("Divisão:",(n1/n2))
